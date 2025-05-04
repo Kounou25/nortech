90809936e895c56d89f38ae5561d983fb743b951
kounou25 <kounougilbert288@gmail.com>
2025-05-04 09:11:01 +0100

    mise en place des migrations (creation de la base de donnee et des tables)

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,15 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/bibliotech', function () {
+Route::get('/libra', function () {
     return view('bibliotech.index');
+});
+
+Route::get('/register', function () {
+    return view('bibliotech.register');
+});
+
+Route::get('/home', function () {
+    return view('bibliotech.home');
 });
 
